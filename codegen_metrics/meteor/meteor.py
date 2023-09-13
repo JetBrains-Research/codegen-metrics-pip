02e0cfd98cd9e1bccf4a081f3e74c575259df1c9
Egor Bogomolov <egor.bogomolov@jetbrains.com>
2023-09-13 22:36:29 +0200

    Change wordnet to zip

--- codegen_metrics/meteor/meteor.py
+++ codegen_metrics/meteor/meteor.py
@@ -2,6 +2,6 @@ import nltk
 from nltk.translate.meteor_score import single_meteor_score as meteor
 
 try:
-    nltk.data.find("corpora/wordnet")
+    nltk.data.find("corpora/wordnet.zip")
 except LookupError:
     nltk.download("wordnet")
